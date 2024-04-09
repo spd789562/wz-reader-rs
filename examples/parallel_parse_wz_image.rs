7fa86f311f1f031842db321e6777ef37c4b747cb
spd789562 <leo.yicun.lin@gmail.com>
2024-04-09 14:33:18 +0800

    [edit] update examples for newer api

--- examples/parallel_parse_wz_image.rs
+++ examples/parallel_parse_wz_image.rs
@@ -1,23 +1,23 @@
 use std::thread;
-use wz_reader::NodeMethods;
-use wz_reader::arc::WzNodeArc;
+use wz_reader::{WzNode, WzNodeArc};
+
 
 fn main() {
-    let node = WzNodeArc::new_wz_file(r"D:\MapleStory\Data\UI\UI_000.wz", None);
+    let node: WzNodeArc = WzNode::from_wz_file(r"D:\MapleStory\Data\UI\UI_000.wz", None).unwrap().into();
 
-    node.parse().unwrap();
 
-    {
-        let node = node.read().unwrap();
-        let handles = node.children.values().map(|node| {
-            let node = node.clone();
-            thread::spawn(move || {
-                node.parse().unwrap();
-            })
-        }).collect::<Vec<_>>();
+    let mut node_write = node.write().unwrap();
+    
+    node_write.parse(&node).unwrap();
 
-        for t in handles {
-            t.join().unwrap();
-        }
+    let handles = node_write.children.values().map(|node| {
+        let node = node.clone();
+        thread::spawn(move || {
+            node.write().unwrap().parse(&node).unwrap();
+        })
+    }).collect::<Vec<_>>();
+
+    for t in handles {
+        t.join().unwrap();
     }
 }
--- examples/wz_to_json.rs
+++ examples/wz_to_json.rs
@@ -1,31 +1,32 @@
 use wz_reader::property::Vector2D;
-use wz_reader::{NodeMethods, WzObjectType, property::WzPropertyType};
-use wz_reader::arc::WzNodeArc;
+use wz_reader::{WzNode, WzNodeArc, WzObjectType, property::WzValue};
 use serde_json::{ Map, Value };
 
-fn walk_node_and_to_json(node_arc: WzNodeArc, json: &mut Map<String, Value>) {
-    node_arc.parse().unwrap();
+fn walk_node_and_to_json(node_arc: &WzNodeArc, json: &mut Map<String, Value>) {
+    {
+        node_arc.write().unwrap().parse(node_arc).unwrap();
+    }
     let node = node_arc.read().unwrap();
     match &node.object_type {
-        WzObjectType::Property => {
-            match &node.property_type {
-                WzPropertyType::Int(value) => {
+        WzObjectType::Value(value_type) => {
+            match value_type {
+                WzValue::Int(value) => {
                     json.insert(node.name.clone(), Value::Number(serde_json::Number::from(*value)));
                 },
-                WzPropertyType::Short(value) => {
+                WzValue::Short(value) => {
                     json.insert(node.name.clone(), Value::Number(serde_json::Number::from(*value)));
                 },
-                WzPropertyType::Long(value) => {
+                WzValue::Long(value) => {
                     json.insert(node.name.clone(), Value::Number(serde_json::Number::from(*value)));
                 },
-                WzPropertyType::Float(value) => {
+                WzValue::Float(value) => {
                     json.insert(node.name.clone(), Value::Number(serde_json::Number::from_f64((*value).into()).unwrap()));
                 },
-                WzPropertyType::Double(value) => {
+                WzValue::Double(value) => {
                     json.insert(node.name.clone(), Value::Number(serde_json::Number::from_f64(*value).unwrap()));
                 },
-                WzPropertyType::String(_) | WzPropertyType::UOL(_) => {
-                    let string = node_arc.get_string();
+                WzValue::String(wz_string) | WzValue::UOL(wz_string) => {
+                    let string = wz_string.get_string();
                     match string {
                         Ok(string) => {
                             json.insert(node.name.clone(), Value::String(string));
@@ -35,49 +36,41 @@ fn walk_node_and_to_json(node_arc: WzNodeArc, json: &mut Map<String, Value>) {
                         }
                     }
                 },
-                WzPropertyType::Vector(Vector2D (x, y)) => {
+                WzValue::Vector(Vector2D (x, y)) => {
                     let mut vec = Map::new();
                     vec.insert("x".to_string(), Value::Number(serde_json::Number::from(*x)));
                     vec.insert("y".to_string(), Value::Number(serde_json::Number::from(*y)));
                     json.insert(node.name.clone(), Value::Object(vec));
                 },
-                WzPropertyType::SubProperty | WzPropertyType::Convex | WzPropertyType::PNG(_) => {
-                    let mut child_json = Map::new();
-                    if node.children.len() != 0 {
-                        for value in node.children.values() {
-                            walk_node_and_to_json(value.clone(), &mut child_json);
-                        }
-                        json.insert(node.name.clone(), Value::Object(child_json));
-                    }
-                },
-                WzPropertyType::Null | WzPropertyType::RawData | WzPropertyType::Sound(_) | WzPropertyType::Lua => {
+                WzValue::Null | WzValue::RawData(_) | WzValue::Lua(_) => {
                     json.insert(node.name.clone(), Value::Null);
                 },
             }
         },
-        WzObjectType::Directory | WzObjectType::Image | WzObjectType::File => {
+        WzObjectType::Directory(_) | WzObjectType::Image(_) | WzObjectType::File(_) | WzObjectType::Property(_) => {
             let mut child_json = Map::new();
             if node.children.len() != 0 {
                 for value in node.children.values() {
-                    walk_node_and_to_json(value.clone(), &mut child_json);
+                    walk_node_and_to_json(value, &mut child_json);
                 }
                 json.insert(node.name.clone(), Value::Object(child_json));
             }
         }
-        _ => {}
     }
 }
 
 fn main() {
     /* resolve single wz file */
-    let node = WzNodeArc::new_wz_file(r"D:\MapleStory\Data\UI\UI_000.wz", None);
+    let node: WzNodeArc = WzNode::from_wz_file(r"D:\MapleStory\Data\UI\UI_000.wz", None).unwrap().into();
 
-    node.parse().unwrap();
+    let mut node_write = node.write().unwrap();
+
+    node_write.parse(&node).unwrap();
 
     let mut json = Map::new();
 
-    for (key, value) in node.read().unwrap().children.iter() {
-        walk_node_and_to_json(value.clone(), &mut json);
+    for value in node_write.children.values() {
+        walk_node_and_to_json(value, &mut json);
     }
 
     let json_string = serde_json::to_string_pretty(&Value::Object(json)).unwrap();
