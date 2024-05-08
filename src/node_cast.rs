076683d617e8ee90fecf2a4da1e58a161b654f26
spd789562 <leo.yicun.lin@gmail.com>
2024-05-08 11:18:06 +0800

    [add] WzNodeCase::try_as_uol and is_sub_property, is_convex

--- src/node_cast.rs
+++ src/node_cast.rs
@@ -23,6 +23,9 @@ pub trait WzNodeCast {
     fn try_as_png(&self) -> Option<&WzPng>;
     fn try_as_sound(&self) -> Option<&WzSound>;
     fn try_as_string(&self) -> Option<&WzString>;
+    fn is_sub_property(&self) -> bool;
+    fn is_convex(&self) -> bool;
+
     fn try_as_lua(&self) -> Option<&WzLua>;
     fn try_as_raw_data(&self) -> Option<&WzRawData>;
 
@@ -32,6 +35,7 @@ pub trait WzNodeCast {
     fn try_as_long(&self) -> Option<&i64>;
     fn try_as_float(&self) -> Option<&f32>;
     fn try_as_double(&self) -> Option<&f64>;
+    fn try_as_uol(&self) -> Option<&WzString>;
 }
 
 macro_rules! try_as {
@@ -83,6 +87,13 @@ impl WzNodeCast for WzNode {
             _ => None,
         }
     }
+    
+    fn is_sub_property(&self) -> bool {
+        matches!(&self.object_type, WzObjectType::Property(WzSubProperty::Property))
+    }
+    fn is_convex(&self) -> bool {
+        matches!(&self.object_type, WzObjectType::Property(WzSubProperty::Convex))
+    }
 
     try_as_wz_value!(try_as_lua, Lua, WzLua);
     try_as_wz_value!(try_as_raw_data, RawData, WzRawData);
@@ -93,6 +104,7 @@ impl WzNodeCast for WzNode {
     try_as_wz_value!(try_as_long, Long, i64);
     try_as_wz_value!(try_as_float, Float, f32);
     try_as_wz_value!(try_as_double, Double, f64);
+    try_as_wz_value!(try_as_uol, UOL, WzString);
 }
 
 #[cfg(test)]
@@ -210,6 +222,32 @@ mod test {
         assert!(node.try_as_string().is_some());
         assert!(node.try_as_file().is_none());
     }
+
+    #[test]
+    fn try_as_uol() {
+        let reader = Arc::new(setup_wz_reader().unwrap());
+        let string = WzString::from_meta(WzStringMeta::empty(), &reader);
+        let node = WzNode::from_str("test", WzObjectType::Value(WzValue::UOL(string)), None);
+
+        assert!(node.try_as_uol().is_some());
+        assert!(node.try_as_file().is_none());
+    }
+
+    #[test]
+    fn is_sub_property() {
+        let node = WzNode::from_str("test", WzObjectType::Property(WzSubProperty::Property), None);
+
+        assert!(node.is_sub_property());
+        assert!(!node.is_convex());
+    }
+    #[test]
+    fn is_convex() {
+        let node = WzNode::from_str("test", WzObjectType::Property(WzSubProperty::Convex), None);
+
+        assert!(node.is_convex());
+        assert!(!node.is_sub_property());
+    }
+
     #[test]
     fn try_as_lua() {
         let reader = Arc::new(setup_wz_reader().unwrap());
