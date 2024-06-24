2ac77885455951d9d2dd5ac215220d5487baa40c
crumblingstatue <radiantstatue@gmail.com>
2024-06-24 19:06:17 +0200

    Remove hardcoded paths from parse_single_img_file example

--- examples/parse_single_img_file.rs
+++ examples/parse_single_img_file.rs
@@ -1,24 +1,23 @@
+use std::path::PathBuf;
+
 use wz_reader::property::get_image;
 use wz_reader::util::walk_node;
-use wz_reader::version::WzMapleVersion;
 use wz_reader::{WzNode, WzNodeArc, WzNodeCast};
 
 fn main() {
+    let mut args = std::env::args_os().skip(1);
+    let path = args.next().expect("Need .wz file as 1st argument");
+    let out_dir: PathBuf = args.next().expect("Need out dir as 2nd argument").into();
     /* resolve single img file */
-    let node: WzNodeArc = WzNode::from_img_file(
-        r"D:\MapleStory\Data\Item\_Canvas\_Canvas\5000024.img",
-        Some(WzMapleVersion::BMS),
-        None,
-    )
-    .unwrap()
-    .into();
+    let node: WzNodeArc = WzNode::from_img_file(path, None, None).unwrap().into();
 
     walk_node(&node, true, &|node: &WzNodeArc| {
         let node_read = node.read().unwrap();
         if node_read.try_as_png().is_some() {
-            let image = get_image(&node).unwrap();
-            let save_name = node_read.get_full_path().replace("/", "-");
-            image.save(format!("./images/{save_name}.png")).unwrap();
+            let image = get_image(&node).unwrap().into_rgba8();
+            let save_name = [&node_read.get_full_path().replace("/", "-"), ".png"].concat();
+            let out_path = out_dir.join(save_name);
+            image.save(out_path).unwrap();
         }
     });
 }
