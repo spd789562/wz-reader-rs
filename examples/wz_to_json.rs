0f9cfa6e33e2e42bbb8d26367f53ecac0c1c88d4
spd789562 <leo.yicun.lin@gmail.com>
2024-09-04 11:51:17 +0800

    [edit] fix wz_to_json exmaple

--- examples/wz_to_json.rs
+++ examples/wz_to_json.rs
@@ -1,9 +1,8 @@
 use serde_json::{Map, Value};
 use wz_reader::{util::node_util, WzNode, WzNodeArc, WzObjectType};
 
-fn walk_node_and_to_json(node_arc: &WzNodeArc, json: &mut Map<String, Value>) {
-    node_util::parse_node(node_arc).unwrap();
-    let node = node_arc.read().unwrap();
+fn walk_node_and_to_json(node: &WzNodeArc, json: &mut Map<String, Value>) {
+    node.parse(node).unwrap();
     match &node.object_type {
         WzObjectType::Value(value_type) => {
             json.insert(node.name.to_string(), value_type.clone().into());
@@ -13,8 +12,9 @@ fn walk_node_and_to_json(node_arc: &WzNodeArc, json: &mut Map<String, Value>) {
         | WzObjectType::File(_)
         | WzObjectType::Property(_) => {
             let mut child_json = Map::new();
-            if node.children.len() != 0 {
-                for value in node.children.values() {
+            let node_children = node.children.read().unwrap();
+            if node_children.len() != 0 {
+                for value in node_children.values() {
                     walk_node_and_to_json(value, &mut child_json);
                 }
                 json.insert(node.name.to_string(), Value::Object(child_json));
@@ -34,15 +34,13 @@ fn main() {
     /* resolve single wz file */
     let node: WzNodeArc = WzNode::from_wz_file(&path, None).unwrap().into();
 
-    let mut node_write = node.write().unwrap();
+    let file_name = node.name.to_string();
 
-    let file_name = node_write.name.to_string();
-
-    node_write.parse(&node).unwrap();
+    node.parse(&node).unwrap();
 
     let mut json = Map::new();
 
-    for value in node_write.children.values() {
+    for value in node.children.read().unwrap().values() {
         walk_node_and_to_json(value, &mut json);
     }
 
